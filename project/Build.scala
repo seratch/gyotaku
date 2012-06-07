1f0dd5616f92485edb53ccab401451bff1f5a4a0
Kazuhiro Sera <seratch@gmail.com>
2012-06-07 20:17:41 +0900

    version 0.1.2

--- project/Build.scala
+++ project/Build.scala
@@ -8,15 +8,16 @@ object AppBuild extends Build {
   lazy val mainSettings: Seq[Project.Setting[_]] = Defaults.defaultSettings ++ Seq(
     organization := "com.github.seratch",
     name := "gyotaku",
-    version := "0.1.0",
+    version := "0.1.2",
     scalaVersion := "2.9.1",
     externalResolvers ~= (_.filter(_.name != "Scala-Tools Maven2 Repository")),
     resolvers += "twitter" at "http://maven.twttr.com/",
     libraryDependencies ++= Seq(
+      "org.scala-lang" % "scala-swing" % "2.9.1",
       "com.twitter" %% "util-eval" % "4.0.1",
       "commons-io" % "commons-io" % "2.3",
       "net.liftweb" %% "lift-json" % "2.4",
-       "org.yaml" % "snakeyaml" % "1.10",
+      "org.yaml" % "snakeyaml" % "1.10",
       "net.sourceforge.htmlcleaner" % "htmlcleaner" % "2.2",
       "org.seleniumhq.selenium" % "selenium-firefox-driver" % "2.22.0",
       "org.scalatest" %% "scalatest" % "1.7.2" % "test"
